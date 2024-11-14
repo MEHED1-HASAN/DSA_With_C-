321a9e520c033a005590e2d2d2ef0f806bb3825f
Mehedi Hasan <ug2102007@cse.pstu.ac.bd>
2024-11-14 19:59:23 +0600

    Update In-order_traversing.cpp

--- Tree_Traversing/In-order_traversing.cpp
+++ Tree_Traversing/In-order_traversing.cpp
@@ -1,1 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+int data;
+struct Node *left,*right;
+Node(int v)
+{
+    data=v;
+    left=right=nullptr;
+}
+};
+void inorder(struct Node* node)
+{
+    if(node==nullptr)
+      return;
+    inorder(node->left);
+    cout<<node->data<<" ";
+    inorder(node->right);
 
+
+}
+
+int main()
+{
+    struct Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+     root->right->right=new Node(6);
+     cout<<"The inorder traversing of the binary tree:";
+     inorder(root);
+
+}
