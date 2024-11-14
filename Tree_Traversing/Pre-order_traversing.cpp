9bb67357bb87666c454f765cf7f81050888707b4
Mehedi Hasan <ug2102007@cse.pstu.ac.bd>
2024-11-14 19:58:01 +0600

    Update Pre-order_traversing.cpp

--- Tree_Traversing/Pre-order_traversing.cpp
+++ Tree_Traversing/Pre-order_traversing.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left,*right;
+    Node(int v)
+    {
+        data=v;
+        left=right=nullptr;
+    }
 
+};
+void preorder(struct Node* node)
+{     if(node==nullptr)
+return;
+cout<<node->data<<"  ";
+preorder(node->left);
+preorder(node->right);
+}
+
+int main()
+{
+    struct Node* root=new Node(1);
+    root->left=new Node(2);
+      root->right = new Node(3);
+   root->left->left=new Node(4);
+    root->left->right=new Node(5);
+   root->right->right=  new Node(6);
+   cout<<"Preorder traversal of binary tree is:";
+   preorder(root);
+
+}
