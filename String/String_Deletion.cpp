08d9d18864dbd0d76dd8477ef90a3f90d4474389
Mehedi Hasan <ug2102007@cse.pstu.ac.bd>
2024-11-15 21:58:37 +0600

    Update String_Deletion.cpp

--- String/String_Deletion.cpp
+++ String/String_Deletion.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string st,pattern;
+    cout<<"Enter the string:";
+    getline(cin,st);
+    cout<<"Enter the pattern you want to delete:";
+    getline(cin,pattern);
+    int ind=st.find(pattern);
+    if(ind==-1)
+    {
+        cout<<"The pattern is not found in the string";
+    }
+    else
+    {
+
+
+    while(ind!=-1)
+    {
+        st.erase(ind,pattern.length());
+
+        ind=st.find(pattern);
+    }
+
+    cout<<st;
+    }
+
+}
 
