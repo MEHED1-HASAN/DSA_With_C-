bcee117022cc43dc9849bdecf7db7eec2a2dc15c
Mehedi Hasan <ug2102007@cse.pstu.ac.bd>
2024-11-16 23:49:41 +0600

    Update String_Replacement.cpp

--- String/String_Replacement.cpp
+++ String/String_Replacement.cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string st,st1,st2;
+    cout<<"Enter the string:";
+    getline(cin,st);
+    cout<<"Enter the pattern you want to replace:";
+    getline(cin,st1);
+    cout<<"Enter the pattern you want to add:";
+        getline(cin,st2);
+    int ind=st.find(st1);
+    if(st2.find(st1) != string::npos)
+    {
+        cout<<"String 1 is a substring of string 2";
 
+    }
+    else
+        {
+
+    while(ind!=-1)
+    {
+        st.replace(ind,st1.length(),st2);
+    /* Or you can use the following two statements
+    st.erase(ind, st1.length());
+    st.insert(ind, st2);*/
+        ind=st.find(st1);
+
+
+    }
+
+    cout<<st;
+        }
+
+}
