f537a5167eda7cb2c104dd5fb57ecef52d4755ae
Mehedi Hasan <ug2102007@cse.pstu.ac.bd>
2024-11-19 22:08:15 +0600

    Update Traversing.cpp

--- Array/Traversing.cpp
+++ Array/Traversing.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int size,i,j;
+   cout<<"Enter the array size:";
+   cin>>size;
+   int a[size];
+   cout<<"Enter the array elements:";
+   for(i=0;i<size;i++)
+   {
+       cin>>a[i];
 
+   }
+   cout<<"The elements are:";
+    for(j=0;j<size;j++)
+   {
+       cout<<a[j]<<"   ";
+
+   }
+
+}
